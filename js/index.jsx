54434ea50456c99f5e9367baa5c2ed05925ff70c
Jon Kuperman <jon.kuperman@gmail.com>
2016-06-10 16:00:09 -0700

    initial redux commit

--- js/index.jsx
+++ js/index.jsx
@@ -1,5 +1,15 @@
 'use strict'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
 import App from './components/App.jsx'
 
-ReactDOM.render(<App />, document.getElementById('content'))
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('content')
+)
